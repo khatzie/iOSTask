f59520aae2cd16c771335e7402eb7225c1489504
Katherine Amar <wog.kcpetalio@gmail.com>
2024-11-13 17:02:42 +0800

    Initial Commit

---
+++ iOSTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSTask
+//
+//  Created by Katherine Petalio on 11/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSTask/iOSTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTaskApp.swift
+//  iOSTask
+//
+//  Created by Katherine Petalio on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct iOSTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
